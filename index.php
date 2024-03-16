e2a3f1d80b9c3b6286a1f02c57d8e24a2f4e5d06
Jerry Vargas <>
2024-03-16 13:29:38 -0400

    no message

--- index.php
+++ index.php
@@ -1,13 +1,28 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-Hello World!
-</body>
-</html>
+<?php
+
+$books = [
+    [
+        'name' => 'Do Androids Dream of Electric Sheep',
+        'author' => 'Philip K. Dick',
+        'releaseYear' => 1968,
+        'purchaseUrl' => 'http://example.com'
+    ],
+    [
+        'name' => 'Project Hail Mary',
+        'author' => 'Andy Weir',
+        'releaseYear' => 2021,
+        'purchaseUrl' => 'http://example.com'
+    ],
+    [
+        'name' => 'The Martian',
+        'author' => 'Andy Weir',
+        'releaseYear' => 2011,
+        'purchaseUrl' => 'http://example.com'
+    ],
+];
+
+$filteredBooks = array_filter($books, function ($book) {
+    return $book['releaseYear'] >= 1950 && $book['releaseYear'] <= 2020;
+});
+
+require "index.view.php";
