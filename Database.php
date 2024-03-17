79a394a5ed82b6bfb82edf2aa8bd5bafdf97aaee
Jerry Vargas <>
2024-03-17 02:24:15 -0400

    no message

--- Database.php
+++ Database.php
@@ -4,13 +4,15 @@ class Database
 {
     public $connection;
 
-    public function __construct()
+//    public function __construct($config, $username = 'root', $password = '')
+// DDEV CONFIG
+    public function __construct($config, $username = 'db', $password = 'db')
     {
-        //$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
-        //DDEV DB connection
-        $dsn = "mysql:host=db;port=3306;dbname=db;user=db;password=db;charset=utf8mb4";
+        $dsn = 'mysql:' . http_build_query($config, '', ';');
 
-        $this->connection = new PDO($dsn);
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
     }
 
     public function query($query)
---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+//    'database' => [
+//        'host' => 'localhost',
+//        'port' => 3306,
+//        'dbname' => 'myapp',
+//        'charset' => 'utf8mb4'
+//    ],
+//DDEV CONFIG
+    'database' => [
+        'host' => 'db',
+        'port' => 3306,
+        'dbname' => 'db',
+        'charset' => 'utf8mb4',
+    ],
+    //
+];
