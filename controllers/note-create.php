c725c6dd15f6e7d3310426494c0d8f8137cee1dc
Jerry Vargas <>
2024-03-18 23:51:47 -0400

    no message

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,17 +1,17 @@
 <?php
-$config = require('config.php');
+
+require 'Validator.php';
+
+$config = require 'config.php';
 $db = new Database($config['database']);
+
 $heading = 'Create Note';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $errors = [];
 
-    if (strlen($_POST['body']) === 0) {
-        $errors['body'] = 'A body is required';
-    }
-
-    if (strlen($_POST['body']) > 1000) {
-        $errors['body'] = 'The body can not be more than 1,000 characters.';
+    if (! Validator::string($_POST['body'], 1, 1000)) {
+        $errors['body'] = 'A body of no more than 1,000 characters is required.';
     }
 
     if (empty($errors)) {
@@ -21,5 +21,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         ]);
     }
 }
-
 require 'views/note-create.view.php';
