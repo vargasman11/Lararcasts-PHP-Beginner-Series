38c5accec718eb597eee86e62d37c7a6a62e5cb1
Jerry Vargas <>
2024-03-18 23:15:28 -0400

    no message

--- Database.php
+++ Database.php
@@ -1,15 +1,17 @@
 <?php
 
+
 class Database
 {
     public $connection;
+    public $statement;
 
-//    public function __construct($config, $username = 'root', $password = '')
-// DDEV CONFIG
+    //public function __construct($config, $username = 'root', $password = '')
+    //DDEV Config
     public function __construct($config, $username = 'db', $password = 'db')
+
     {
         $dsn = 'mysql:' . http_build_query($config, '', ';');
-
         $this->connection = new PDO($dsn, $username, $password, [
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]);
@@ -17,10 +19,31 @@ class Database
 
     public function query($query, $params = [])
     {
-        $statement = $this->connection->prepare($query);
+        $this->statement = $this->connection->prepare($query);
 
-        $statement->execute($params);
+        $this->statement->execute($params);
 
-        return $statement;
+        return $this;
+    }
+
+    public function get()
+    {
+        return $this->statement->fetchAll();
+    }
+
+    public function find()
+    {
+        return $this->statement->fetch();
+    }
+
+    public function findOrFail()
+    {
+        $result = $this->find();
+
+        if (!$result) {
+            abort();
+        }
+
+        return $result;
     }
 }
--- controllers/note.php
+++ controllers/note.php
@@ -1,19 +1,14 @@
 <?php
-$db = new Database($config['database']);
 
+$config = require('config.php');
+$db = new Database($config['database']);
 $heading = 'Note';
 $currentUserId = 1;
 
 $note = $db->query('select * from notes where id = :id', [
-'id' => $_GET['id']
-])->fetch();
+    'id' => $_GET['id']
+])->findOrFail();
 
-if (! $note) {
-abort();
-}
-
-if ($note['user_id'] !== $currentUserId) {
-abort(Response::FORBIDDEN);
-}
+authorize($note['user_id'] === $currentUserId);
 
 require "views/note.view.php";
