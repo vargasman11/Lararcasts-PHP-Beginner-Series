4dfd58158c853e77cd69d3cc21e5ec26ef5b34d5
Jerry Vargas <>
2024-03-19 00:13:47 -0400

    no message

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -4,11 +4,10 @@ $config = require('config.php');
 $db = new Database($config['database']);
 $heading = 'Note';
 $currentUserId = 1;
-
 $note = $db->query('select * from notes where id = :id', [
     'id' => $_GET['id']
 ])->findOrFail();
 
 authorize($note['user_id'] === $currentUserId);
 
-require "views/note.view.php";
+require "views/notes/show.view.php";
