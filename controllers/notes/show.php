6d1a2fe9b29b43a12703cf09b30fe2a17bbf9583
Jerry Vargas <>
2024-03-21 00:28:55 -0400

    no message

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -5,14 +5,38 @@ use Core\Database;
 $config = require base_path('config.php');
 $db = new Database($config['database']);
 
-$currentUserId = 1;
-$note = $db->query('select * from notes where id = :id', [
-    'id' => $_GET['id']
-])->findOrFail();
-//authorize($note['user_id'] === $currentUserId);
-// Still need to see why it using string to int comparison
-authorize($note['user_id'] === $currentUserId);
-view("notes/show.view.php", [
-    'heading' => 'Note',
-    'note' => $note
-]);
+$currentUserId = 25;
+//$currentUserId = 1;
+
+// Kinda gross, yes? We'll refactor toward a cleaner approach in episode 33.
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $note = $db->query('select * from notes where id = :id', [
+        'id' => $_GET['id']
+//        $_GET['id'] should be replaced with $_POST['id']
+    ])->findOrFail();
+
+    //authorize($note['user_id'] === $currentUserId);
+    // need to figure out comparison, ongoing technical debt
+    authorize($note['user_id'] == $currentUserId);
+
+    $db->query('delete from notes where id = :id', [
+        'id' => $_GET['id']
+        //$_GET['id'] should be replaced with $_POST['id']
+    ]);
+
+    header('location: /notes');
+    exit();
+} else {
+    $note = $db->query('select * from notes where id = :id', [
+        'id' => $_GET['id']
+    ])->findOrFail();
+
+    //authorize($note['user_id'] === $currentUserId);
+    // need to figure out comparison, ongoing technical debt
+    authorize($note['user_id'] == $currentUserId);
+
+    view("notes/show.view.php", [
+        'heading' => 'Note',
+        'note' => $note
+    ]);
+}
