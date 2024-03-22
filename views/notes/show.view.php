ae8b2c8e77baa2a1833af456846f30725c219922
Jerry Vargas <>
2024-03-22 00:27:47 -0400

    no message

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -6,13 +6,13 @@
             <p class="mb-6">
                 <a href="/notes" class="text-blue-500 underline">go back...</a>
             </p>
+
             <p><?= htmlspecialchars($note['body']) ?></p>
 
-            <form class="mt-6" method="POST">
-                <input type="hidden" name="_method" value="DELETE">
-                <input type="hidden" name="id" value="<?= $note['id'] ?>">
-                <button class="text-sm text-red-500">Delete</button>
-            </form>
+            <footer class="mt-6">
+                <a href="/note/edit?id=<?= $note['id'] ?>" class="inline-flex justify-center rounded-md border border-transparent bg-gray-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Edit</a>
+            </footer>
         </div>
     </main>
+
 <?php require base_path('views/partials/footer.php') ?>
