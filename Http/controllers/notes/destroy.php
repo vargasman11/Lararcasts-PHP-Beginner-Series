820a11fbef5e8735ce5255bfc8bc526cb9a42938
Jerry Vargas <>
2024-03-23 01:55:41 -0400

    no message

--- Http/controllers/notes/destroy.php
+++ Http/controllers/notes/destroy.php
@@ -10,11 +10,12 @@ $currentUserId = 1;
 $note = $db->query('select * from notes where id = :id', [
     'id' => $_POST['id']
 ])->findOrFail();
-//authorize($note['user_id'] === $currentUserId);
-//Technical debt comparison
-authorize($note['user_id'] == $currentUserId);
+
+authorize($note['user_id'] === $currentUserId);
+
 $db->query('delete from notes where id = :id', [
     'id' => $_POST['id']
 ]);
+
 header('location: /notes');
 exit();
