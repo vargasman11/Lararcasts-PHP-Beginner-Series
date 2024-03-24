bb6983ad28b6f5e36529704d982968ec0fb80537
Jerry Vargas <>
2024-03-24 16:31:40 -0400

    no message

--- public/index.php
+++ public/index.php
@@ -1,9 +1,10 @@
 <?php
 
+use Core\Session;
+
 session_start();
 
 const BASE_PATH = __DIR__.'/../';
-
 require BASE_PATH.'Core/functions.php';
 spl_autoload_register(function ($class) {
     $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
@@ -14,4 +15,7 @@ $router = new \Core\Router();
 $routes = require base_path('routes.php');
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+
 $router->route($uri, $method);
+
+Session::unflash();
