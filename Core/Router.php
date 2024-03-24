3452dec9c7460c93aa58018ab1f7161a675eb5e3
Jerry Vargas <>
2024-03-24 18:15:39 -0400

    moving up to PHP 8.1

--- Core/Router.php
+++ Core/Router.php
@@ -1,11 +1,15 @@
 <?php
+
 namespace Core;
+
 use Core\Middleware\Authenticated;
 use Core\Middleware\Guest;
 use Core\Middleware\Middleware;
+
 class Router
 {
     protected $routes = [];
+
     public function add($method, $uri, $controller)
     {
         $this->routes[] = [
@@ -16,26 +20,32 @@ class Router
         ];
         return $this;
     }
+
     public function get($uri, $controller)
     {
         return $this->add('GET', $uri, $controller);
     }
+
     public function post($uri, $controller)
     {
         return $this->add('POST', $uri, $controller);
     }
+
     public function delete($uri, $controller)
     {
         return $this->add('DELETE', $uri, $controller);
     }
+
     public function patch($uri, $controller)
     {
         return $this->add('PATCH', $uri, $controller);
     }
+
     public function put($uri, $controller)
     {
         return $this->add('PUT', $uri, $controller);
     }
+
     public function only($key)
     {
         $this->routes[array_key_last($this->routes)]['middleware'] = $key;
@@ -54,6 +64,12 @@ class Router
 
         $this->abort();
     }
+
+    public function previousUrl()
+    {
+        return $_SERVER['HTTP_REFERER'];
+    }
+
     protected function abort($code = 404)
     {
         http_response_code($code);
